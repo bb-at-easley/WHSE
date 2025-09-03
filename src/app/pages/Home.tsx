0297bf1cf108ced088b42ee3143bb49150693b75
Barrett Burnworth <barrettburnworth@gmail.com>
2025-09-03 11:24:37 -0500

    login and function delete

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -4,8 +4,8 @@ export function Home({ ctx }: RequestInfo) {
   return (
     <div>
       <p>
-        {ctx.user?.username
-          ? `You are logged in as user ${ctx.user.username}`
+        {ctx.user?.email
+          ? `You are logged in as user ${ctx.user.email}`
           : "You are not logged in"}
       </p>
     </div>
